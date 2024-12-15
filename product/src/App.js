0a43d9016861e75e528a65ac39cd4a8de4985cf4
Artube1991 <artube1991@gmail.com>
2024-12-15 18:31:08 +0200

    created roots and made them work

--- product/src/App.js
+++ product/src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import New from './components/New';
+import Classics from './components/Classics';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <>
     <nav>
-      <h1>KLYUCHI STORE</h1>
-      <ul>
-        <li>NEW!</li>
-        <li>Our Classics</li>
-        <li>About</li>
-        <li>Contact</li>
-        <img src="" alt="User Picture"/>
-      </ul>
+    <p className="website-logo">KEYS / КЛЮЧИ </p>
+    <ul>
+      <li><Link to="/">NEW!</Link></li>
+      <li><Link to="/classic">Our Classics</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+      <li><Link to="/about">About</Link></li>
+    </ul>
     </nav>
+    <Routes>      
+      <Route exact path="/" element={<New />}/>
+      <Route path="/classic" element={<Classics />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
     <div className="main">
       <h1>something</h1>
     </div>
