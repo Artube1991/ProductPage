0cab2421fb233e9236c8bfdb3a0d1733c754018c
Artube1991 <artube1991@gmail.com>
2024-12-15 17:24:01 +0200

    started my new project

---
+++ product/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <nav>
+      <h1>KLYUCHI STORE</h1>
+      <ul>
+        <li>NEW!</li>
+        <li>Our Classics</li>
+        <li>About</li>
+        <li>Contact</li>
+        <img src="" alt="User Picture"/>
+      </ul>
+    </nav>
+    <div className="main">
+      <h1>something</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
