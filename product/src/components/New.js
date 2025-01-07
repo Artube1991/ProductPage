ed311cc819db72ff46f37c2da8709e6f88d3384b
Artube1991 <artube1991@gmail.com>
2025-01-07 21:37:05 +0200

    wrote styles for product info box

--- product/src/components/New.js
+++ product/src/components/New.js
@@ -2,22 +2,19 @@ import { KeysBooksContext } from "../App";
 // import { KnightsBooksContext } from "../App";
 import { useState, useContext, useEffect } from "react";
 
-
 const New = (props) => {
   const [keysBooks, setKeysBooks] = useContext(KeysBooksContext);
+  const book = props.book;
   const [keysBooksChosen, setKeysBooksChosen] = useState(0);
 //   const [knightsBooks, setKnightsBooks] = useContext(KnightsBooksContext);
 
-const book = props.book;
+const pictureTypesAll = ["main", "overall", "illustration", "text"];
 const [pictureType, setPictureType] = useState("main");
-
-let picturePath = `./media/${book}-${pictureType}.jpg`;
+const picturePath = `./media/${book}-${pictureType}.jpg`;
 const smallProductPicture = document.getElementsByClassName("small-product-picture");
 
-const pictureTypesAll = ["main", "overall", "illustration", "text"];
 const [picturePathCarousel, setPicturePathCarousel] = useState("");
 const [pictureCarouselIndex, setPictureCarouselIndex] = useState(Number);
-
 const [carouselIsActive, setCarouselIsActive] = useState(false);
 
 const [cartMessage, setCartMessage] = useState("");
@@ -153,7 +150,7 @@ const deletingCartMessage = () => {
 };
 
     return(
-    <>
+    <div className="product">
     <section className="product-pictures-box">
     <img className="product-picture" src={picturePath} width="400" height="400" onClick={(e) => gettingTheIndexOfPictureCarousel()}/>
     <div className="pictures-row" id="ff">
@@ -166,14 +163,18 @@ const deletingCartMessage = () => {
     <div className="product-info-box">
         <p className="company-title">KEYS: THE LITERATURE CLUB</p>
         <h1 className="product-title">'Keys and Castles': Limited Edition 2019</h1>
-        <p className="product-info">Lorem ipsum</p>
-        <p className="current-price">$6</p>
-        <p className="discount-size">30%</p>
+        <p className="product-info">Lorem ipsum para bellum!!!!!!!!!</p>
+        <p className="current-price">$4<span className="discount-size">30%</span></p>
         <p className="old-price">$6</p>
+        <div className="cart-buttons">
         <button className="button-minus" onClick={(e) => changingAmount("minus")}>-</button>
         <p className="product-amount">{keysBooksChosen}</p>
         <button className="button-plus" onClick={(e) => changingAmount("plus")}>+</button>
-        <button className="add-cart" onClick={(e) => changingCart()}>Add to cart</button>
+        <button className="add-cart" onClick={(e) => changingCart()}>
+        <i class="fa-solid fa-cart-shopping"></i>
+          <span className="cart-button-text">Add to cart</span>
+          </button>
+        </div>
         <p className="cart-message">{cartMessage}</p>
     </div>
     <div className="carousel">
@@ -182,7 +183,7 @@ const deletingCartMessage = () => {
     <img src={picturePathCarousel} width="700" height="700"/>
     <i class="fa-solid fa-caret-right arrows" id="right-arrow" onClick={(e) => channgingPictureCarousel("plus")}></i>
     </div>
-    </>)
+    </div>)
 };
 
 export default New;
