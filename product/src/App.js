1d23a7f788af94e2a72ef2e41499ac2c2b3193ec
Artube1991 <artube1991@gmail.com>
2025-01-10 16:05:14 +0200

    finished with cart design and logic

--- product/src/App.js
+++ product/src/App.js
@@ -18,7 +18,7 @@ function App() {
   // const [knightsAmount, setKnightsAmount] = useState(0);
   const [cartMessage, setCartMessage] = useState("Your cart is empty!");
   const [cartIsVisible, setCartIsVisible] = useState(false);
-
+  const [checkoutLink, setCheckoutLink] = useState("https://vk.com/market-57829582");
 
   const cartIcon = document.querySelector(".cart-icon");
   const [cartKeysAmount, setCartKeysAmount] = useState("");
@@ -27,21 +27,23 @@ function App() {
   const menuItemsStyle = {color: 'gray', textDecoration: 'none', fontFamily: 'Cooper Black'};
 
   const showCart = () => {
-    const cart = document.getElementById("cart");
     if (cartIsVisible === false) {
       setCartIsVisible(true);
       // cartRef.current.setAttribute("style", "display: block");
-      cart.setAttribute("style", "display: block");
     } else if (cartIsVisible === true) {
       setCartIsVisible(false);
-      cart.setAttribute("style", "display: none");
       // cartRef.current.setAttribute("style", "display: none");
     }
+  };
+  
+  const closingCart = () => {
+    const cart = document.querySelector(".cart");
+    setCartIsVisible(false);
   };
 
   useEffect(() => {
     cartUpdate();
-  }, [keysBooks, knightsBooks]);
+  }, [keysBooks, knightsBooks, cartIsVisible]);
 
   useEffect(() => {
     const userpic = document.getElementById("userpic");
@@ -50,21 +52,35 @@ function App() {
   }, []);
 
   const cartUpdate = () => {
+    const cart = document.querySelector(".cart");
     const keysCartBox = document.querySelector(".keys-cart-box");
     const knightsCartBox = document.querySelector(".knights-cart-box");
-  if (keysBooks !== 0 && knightsBooks !== 0) {
+    const checkout = document.querySelector(".checkout");
+    const removingCart = document.querySelector(".removing-cart");
+
+
+  if (cartIsVisible === true) {
+    cart.setAttribute("style", "display: block;");
+    
+    if (keysBooks !== 0 && knightsBooks !== 0) {
     setCartMessage(`You're going to buy ${keysBooks} books 'Keys and Castles' and ${knightsBooks} books 'The Knight of Inspiration'`);
     setCartKeysAmount(keysBooks);
     setCartKnightsAmount(knightsBooks);
     keysCartBox.setAttribute("style", "display: block;");
     knightsCartBox.setAttribute("style", "display: block;")
+    checkout.setAttribute("style", "display: block;");
+    removingCart.setAttribute("style", "display: block;");
+    setCheckoutLink("https://vk.com/market-57829582");
     }
   else if (keysBooks !==0 && knightsBooks < 1) {
     setCartMessage(`You're going to buy ${keysBooks} books 'Keys and Castles'`);
     setCartKeysAmount(keysBooks);
     setCartKnightsAmount("");
     keysCartBox.setAttribute("style", "display: block;");
-    knightsCartBox.setAttribute("style", "display: none;")
+    knightsCartBox.setAttribute("style", "display: none;");
+    checkout.setAttribute("style", "display: block;");
+    removingCart.setAttribute("style", "display: block;");
+    setCheckoutLink("https://vk.com/market/product/klyuchi-i-zamki-57829582-1945734");
     }
   else if (keysBooks < 1 && knightsBooks !== 0) {
     setCartMessage(`You're going to buy ${knightsBooks} books 'The Knight of Inspiration'`);
@@ -72,6 +88,9 @@ function App() {
     setCartKnightsAmount(knightsBooks);
     keysCartBox.setAttribute("style", "display: none;");
     knightsCartBox.setAttribute("style", "display: block;");
+    checkout.setAttribute("style", "display: block;");
+    removingCart.setAttribute("style", "display: block;");
+    setCheckoutLink("https://vk.com/market/product/rytsari-vdokhnovenia-2-voskhozhdenie-57829582-3405002");
     }
   else if (keysBooks < 1 && keysBooks < 1) {
     setCartKeysAmount("");
@@ -79,8 +98,19 @@ function App() {
     setCartMessage("Your cart is empty!");
     keysCartBox.setAttribute("style", "display: none;");
     knightsCartBox.setAttribute("style", "display: none;");
+    checkout.setAttribute("style", "display: none;");
+    removingCart.setAttribute("style", "display: none;");
+    setCheckoutLink("https://vk.com/market-57829582");
     }
+  } else if (cartIsVisible === false) {
+    cart.setAttribute("style", "display: none;");
+  }
   };
+
+  const removeAll = () => {
+    setKeysBooks(0);
+    setKnightBooks(0);
+  }
 
   // const smileChecking = () => {
   //   const userpic = document.getElementById("userpic");
@@ -125,20 +155,24 @@ function App() {
     </Routes>
     </KnightsBooksContext.Provider>
     </KeysBooksContext.Provider>
-    <div id="cart" ref={cartRef}>
+    <div className="cart" ref={cartRef}>
+    <i class="fa-solid fa-xmark cross-little" onClick={(e) => closingCart()}></i>
       <div className="keys-cart-box">
       <img className="keys-picture" src="./media/keys-main.jpg" width="50px" height="50px" alt="Keys and Castles"/>
-      <span className="product-amount-cart">×</span>
+      <span className="product-amount-cart">$4 ×</span>
       <span className="product-amount-cart">{cartKeysAmount}</span>
       </div>
       <div className="knights-cart-box">
       <img className="knights-picture" src="./media/knights-main.jpg" width="50px" height="50px" alt="The Knights of Inspiration"/>
-      <span className="product-amount-cart">×</span>
+      <span className="product-amount-cart">$3 ×</span>
       <span className="product-amount-cart">{cartKnightsAmount}</span>
       </div>
       <p>{cartMessage}</p>
-      {/* <button className="checkout">PROCEED TO CHECKOUT</button>
-      <button className="removing-cart">REMOVE THE ITEMS</button> */}
+      <p className="subtotal">SUBTOTAL: ${4 * keysBooks + 3 * knightsBooks}</p>
+      <div className="cart-handler-buttons">
+      <a className="checkout" href={checkoutLink} target='_blank'> CHECKOUT </a>
+      <button className="removing-cart" onClick={(e) => removeAll()}>REMOVE ALL</button>
+      </div>
       </div>
     <div className="main">
       <h1>something</h1>
